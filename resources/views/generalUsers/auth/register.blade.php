a552c83ba10ee998638cef3df7cd59171dc1b17c
Abdullah <abdullahwaseem.4401@gmail.com>
2025-01-30 20:58:16 +0500

    working on general user

---
+++ resources/views/generalUsers/auth/register.blade.php
@@ -0,0 +1,104 @@
+@extends('drivers.auth.app')
+
+@section('content')
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo">
+                                <img src="{{ asset('assets/images/logo.svg') }}">
+                            </div>
+                            <h4>New here?</h4>
+                            <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
+                            <form class="pt-3" enctype="multipart/form-data" method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <input type="hidden" name="role" id="role" value="generalUser">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg @error('name') is-invalid @enderror" name="name" id="fullName"
+                                        placeholder="Full Name" value="{{ old('name') }}" required>
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" id="email"
+                                        placeholder="Email" value="{{ old('email') }}" required>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" id="password"
+                                        placeholder="Password" required>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg @error('password_confirmation') is-invalid @enderror" name="password_confirmation" id="password_confirmation"
+                                        placeholder="Confirm Password" required>
+                                    @error('password_confirmation')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg @error('phone') is-invalid @enderror" name="phone" id="phone"
+                                        placeholder="Phone" value="{{ old('phone') }}" required>
+                                    @error('phone')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg @error('address') is-invalid @enderror" name="address" id="address"
+                                        placeholder="Address" value="{{ old('address') }}" required>
+                                    @error('address')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="date" class="form-control form-control-lg @error('date_of_birth') is-invalid @enderror" name="date_of_birth" id="date_of_birth"
+                                        placeholder="Date of Birth" value="{{ old('date_of_birth') }}" required>
+                                    @error('date_of_birth')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                
+                                
+                                <div class="mb-4">
+                                    <div class="form-check">
+                                        <label class="form-check-label text-muted">
+                                            <input type="checkbox" class="form-check-input" name="terms_approved" required> I agree to all Terms &
+                                            Conditions
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="mt-3 d-grid gap-2">
+                                    <button type="submit"
+                                        class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn">REGISTER</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+@endsection
