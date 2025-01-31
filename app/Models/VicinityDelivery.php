9aa9379412ad4c4cbcf19ae9dd1d4b84ea2f0c0c
Abdullah <abdullahwaseem.4401@gmail.com>
2025-02-01 02:51:09 +0500

    start working on vicinity delivery

---
+++ app/Models/VicinityDelivery.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VicinityDelivery extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'departure_address',
+        'arrival_address',
+        'transaction_date',
+        'total_price',
+        'status',
+        'payment_status',
+        'payment_details',
+        'payment_method',
+        'accepted',
+        'driver_id' 
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function packageDetails()
+    {
+        return $this->hasMany(PackageDetail::class, 'delivery_id');
+    }
+
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class);
+    }
+}
