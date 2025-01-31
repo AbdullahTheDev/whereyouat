4d5a4b4e0e4584a71dca29406e2d01c95885cf2b
Abdullah <abdullahwaseem.4401@gmail.com>
2025-01-31 21:18:58 +0500

    stoping from driver side

--- resources/views/drivers/delivery/distance_delivery.blade.php
+++ resources/views/drivers/delivery/distance_delivery.blade.php
@@ -19,7 +19,7 @@
                                     </div>
                                     <div class="d-flex align-items-center text-muted font-weight-light">
                                         <i class="mdi mdi-clock icon-sm me-2"></i>
-                                        <span>{{ \Carbon\Carbon::parse($delivery->transation_date)->diffForHumans() }}</span>
+                                        <span>{{ \Carbon\Carbon::parse($delivery->transation_date)->format('Y-m-d h:i A') }}</span>
                                     </div>
                                 </div>
                                 @foreach ($delivery->packageDetails as $packageDetail)
@@ -27,7 +27,16 @@
                                         <img src="{{ asset('assets/images/faces/face3.jpg') }}"
                                             class="img-sm rounded-circle me-3" alt="image">
                                         <div class="mb-0 flex-grow">
-                                            <h5 class="me-2 mb-2">{{ $packageDetail->title }}</h5>
+                                            <div>
+                                                <h5 class="me-2 mb-2">{{ strtoupper(str_replace('_', ' ', $packageDetail->package_type)) }}</h5>
+                                                <span>
+                                                    @if($packageDetail->package_type == 'mini_carton' || $packageDetail->package_type == 'other')
+                                                    Weight: {{ $packageDetail->qty }} KG
+                                                    @else
+                                                    Quantiry: {{ $packageDetail->qty }}
+                                                    @endif
+                                                </span>
+                                            </div>
                                             <p class="mb-0 font-weight-light">{{ $packageDetail->description }}</p>
                                         </div>
                                         <div class="ms-auto">
