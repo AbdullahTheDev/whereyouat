6bac9a2d2682cad6c4508cacef7705134836dcaa
Abdullah <abdullahwaseem.4401@gmail.com>
2025-02-07 23:28:12 +0500

    ftng

--- app/Http/Middleware/partnerHomeMiddleware.php
+++ app/Http/Middleware/partnerHomeMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class partnerHomeMiddleware
@@ -15,6 +16,10 @@ class partnerHomeMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->role === 'partner_home') {
+            return $next($request);
+        }
+
+        return redirect()->route('home')->with('error', 'Access Denied: Partner Home only.');
     }
 }
