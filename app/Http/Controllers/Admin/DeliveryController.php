486020a66594b85f4c24e452d1f6c7bd735dd180
AbdullahTheDev <abdullahwaseem.4401@gmail.com>
2025-02-20 22:32:10 +0500

    wohsgohd

--- app/Http/Controllers/Admin/DeliveryController.php
+++ app/Http/Controllers/Admin/DeliveryController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Business;
 use App\Models\DistanceDelivery;
+use App\Models\Driver;
+use App\Models\PartnerHome;
+use App\Models\User;
 use App\Models\VicinityDelivery;
+use Exception;
 use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
@@ -23,5 +28,72 @@ class DeliveryController extends Controller
     function vicinityDelivery(){
         $deliveries = VicinityDelivery::all();
         return view('admin.deliveries.vicinity.index', compact('deliveries'));
+    }
+
+
+    function distanceDriver($id)
+    {
+        try {
+            $delivery = DistanceDelivery::findOrFail($id);
+            if (!$delivery) {
+                return redirect()->back()->with('error', 'Delivery not found');
+            }
+            $user = User::findOrFail($delivery->driver_id);
+            if (!$user) {
+                return redirect()->back()->with('error', 'Driver not found');
+            }
+            $driver = Driver::where('user_id', $user->id)->first();
+            if (!$driver) {
+                return redirect()->back()->with('error', 'Driver not found');
+            }
+            return view('admin.deliveries.driver.driver_info', compact('driver'));
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
+    function vicinityDriver($id)
+    {
+        try {
+            $delivery = VicinityDelivery::findOrFail($id);
+            if (!$delivery) {
+                return redirect()->back()->with('error', 'Delivery not found');
+            }
+            $user = User::findOrFail($delivery->driver_id);
+            if (!$user) {
+                return redirect()->back()->with('error', 'Driver not found');
+            }
+            $driver = Driver::where('user_id', $user->id)->first();
+            if (!$driver) {
+                return redirect()->back()->with('error', 'Driver not found');
+            }
+            return view('admin.deliveries.driver.driver_info', compact('driver'));
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
+    function distancePartner($id)
+    {
+        try {
+            $delivery = DistanceDelivery::findOrFail($id);
+            if (!$delivery) {
+                return redirect()->back()->with('error', 'Delivery not found');
+            }
+            
+            $user = User::findOrFail($delivery->driver_id);
+            if (!$user) {
+                return redirect()->back()->with('error', 'Driver not found');
+            }
+            if ($user->role == 'partner_home') {
+                $relay = PartnerHome::where('user_id', $user->id)->first();
+            } else {
+                $relay = Business::where('user_id', $user->id)->first();
+            }
+            if (!$relay) {
+                return redirect()->back()->with('error', 'Partner not found');
+            }
+            return view('admin.deliveries.partners.partner_info', compact('relay'));
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
     }
 }
