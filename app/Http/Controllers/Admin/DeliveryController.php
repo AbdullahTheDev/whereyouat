ae1e323c098c9647b44557f60dc5f7bb061d3dba
AbdullahTheDev <abdullahwaseem.4401@gmail.com>
2025-02-20 22:13:58 +0500

    working on admin side

--- app/Http/Controllers/Admin/DeliveryController.php
+++ app/Http/Controllers/Admin/DeliveryController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\DistanceDelivery;
+use App\Models\VicinityDelivery;
 use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
 {
     function index(){
         return view('admin.delivery.index');
+    }
+    function distanceDelivery(){
+        $deliveries = DistanceDelivery::all();
+        return view('admin.deliveries.distance.direct.index', compact('deliveries'));
+    }
+    function vicinityDelivery(){
+        $deliveries = VicinityDelivery::all();
+        return view('admin.deliveries.vicinity.direct.index', compact('deliveries'));
     }
 }
